41364af04a28cb79c6ae00f7758f98cef095d20c
gengxiebo <gengxiebo.ly@bytedance.com>
2024-05-13 21:01:15 +0800

    fix

--- main.go
+++ main.go
@@ -8,26 +8,23 @@ import (
 )
 
 func main() {
-	GinHttps(true) // 这里false 表示 http 服务，非 https
+	GinHttps()
 }
 
-func GinHttps(isHttps bool) error {
+func GinHttps() error {
 
 	r := gin.Default()
 	r.GET("/test", func(c *gin.Context) {
 		c.String(200, "test for 【%s】", "https")
 	})
 
-	if isHttps {
-		r.Use(TlsHandler(443))
+	// r.Use(TLSHandler(443))
 
-		return r.RunTLS(":"+strconv.Itoa(443), "./ssh/luoye-g.top.pem", "./ssh/luoye-g.top.rsa.key")
-	}
-
-	return r.Run(":" + strconv.Itoa(443))
+	// return r.RunTLS(":"+strconv.Itoa(443), "./ssh/luoye-g.top.pem", "./ssh/luoye-g.top.rsa.key")
+	return r.Run("80")
 }
 
-func TlsHandler(port int) gin.HandlerFunc {
+func TLSHandler(port int) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		secureMiddleware := secure.New(secure.Options{
 			SSLRedirect: true,
@@ -39,7 +36,6 @@ func TlsHandler(port int) gin.HandlerFunc {
 		if err != nil {
 			return
 		}
-
 		c.Next()
 	}
 }
